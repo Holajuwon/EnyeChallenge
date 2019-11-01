b1f9614a578f8a5ccabb99d734ae2fa5635c6565
hola <henrijay00@gmail.com>
2019-11-01 20:13:17 +0100

    made a few edits

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,7 +1,7 @@
 import { put, call, fork, take, all } from "redux-saga/effects";
 import { eventChannel } from "redux-saga";
 import { postApiHandler } from "../api";
-import {  updateUser } from "../actions/user";
+import { updateUser } from "../actions/user";
 import firebase from "firebase";
 import appConfig from "../config";
 
@@ -9,8 +9,6 @@ firebase.initializeApp(appConfig);
 const database = firebase.database();
 
 function createEventChannel() {
-  // yield delay(5000)
-  // create an event listener that fires whenever a new user is added to the db
   const listener = eventChannel(emit => {
     database.ref("/users").on("value", data => {
       return emit(data.val() || {});
@@ -21,16 +19,10 @@ function createEventChannel() {
   return listener;
 }
 
-console.log(createEventChannel());
-// call the event listener to add a user upon recieving new users
 function* updateItemSaga() {
-  // create the event listener
   const updateChannel = createEventChannel();
-  // concurently get data from the event listener anytime it is fired
   while (true) {
     const item = yield take(updateChannel);
-    console.log(item);
-    // dispatch the addUser action for the batch of new users
     yield put(updateUser(Object.values(item)));
   }
 }
